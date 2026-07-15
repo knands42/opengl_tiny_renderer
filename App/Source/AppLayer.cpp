2728af78c1de14c941bf88ede2219b7addb74094
Caio Fernandes <caiow.wk2@proton.me>
2026-07-15 23:07:26 +0100

    feat: create app layer skeleton

--- App/Source/AppLayer.cpp
+++ App/Source/AppLayer.cpp
@@ -1,8 +1,28 @@
 #include "AppLayer.h"
 
 #include <cassert>
+#include <print>
 
 namespace App
 {
+    AppLayer::AppLayer()
+    {
+        std::println("AppLayer");
+    }
 
+    AppLayer::~AppLayer()
+    {
+    }
+
+    void AppLayer::OnEvent(Core::Event& event)
+    {
+    }
+
+    void AppLayer::OnUpdate(float ts)
+    {
+    }
+
+    void AppLayer::OnRender()
+    {
+    }
 } // namespace App
--- App/Source/AppLayer.h
+++ App/Source/AppLayer.h
@@ -2,11 +2,24 @@
 
 #include <glad/gl.h>
 
+#include "Events/Event.h"
 #include "Layer.h"
 
 namespace App
 {
     class AppLayer : public Core::Layer
     {
+        public:
+            AppLayer();
+            ~AppLayer();
+
+            virtual void OnEvent(Core::Event& event) override;
+            virtual void OnUpdate(float ts) override;
+
+            virtual void OnRender() override;
+        private:
+            uint32_t m_Shader = 0;
+            uint32_t m_VertexArray = 0;
+            uint32_t m_VertexBuffer = 0;
     };
 } // namespace App
